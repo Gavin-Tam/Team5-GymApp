9626600ba641704b5414642266bad509f2698507
Gavin-Tam <gavintamnguyen2002@outlook.com>
2023-10-31 16:40:30 -0400

    Have added a menu to the app (Navigation)

--- app/src/main/java/com/example/gym/HomeScreenActivity.kt
+++ app/src/main/java/com/example/gym/HomeScreenActivity.kt
@@ -1,11 +1,45 @@
 package com.example.gym
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 
 class HomeScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_screen)
     }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return true
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.profile -> {
+                // Handle the "Profile" menu item
+                val intent = Intent(this, UserProfileActivity::class.java)
+                startActivity(intent)
+                return true
+            }
+            R.id.customer_support -> {
+                val intent = Intent(this, CustomerSupportActivity::class.java)
+                startActivity(intent)
+                return true
+            }
+            R.id.sign_out -> {
+                val intent = Intent(this, LoginScreenActivity::class.java)
+                startActivity(intent)
+                return true
+            }
+            else -> return super.onOptionsItemSelected(item)
+        }
+    }
 }
+
+
+
+
+
